5a1951351bc2fbdeca8ffc1c230f15f96071473d
Carlos Ramirez <carami21@asu.edu>
2025-10-17 23:13:06 +0000

    Now made it custom to greet whoever the user is using the github username

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+import argparse
+import os
 
 def main():
-    print("Hello World!")
+    parser = argparse.ArgumentParser(description="Greet a user.")
+    parser.add_argument("--name", "-n", help="Name to greet")
+    args = parser.parse_args()
+
+    name = args.name or os.getenv("USER") or "World"
+    print(f"Hello, {name}!")
 
 if __name__ == "__main__":
     main()
